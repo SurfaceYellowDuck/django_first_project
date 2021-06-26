243dcd01bea1be86800bfee2514bf3574d860b7d
Nikita <lukonenko.nikitka@mail.ru>
2021-06-26 18:08:07 +0300

    v2.0 commit

---
+++ geekshop/geekshop/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+import json
+page_header = [
+        {'page_name': 'contacts', 'header': 'НАШИ КОНТАКТЫ'},
+        {'page_name': 'index', 'header': 'УДОБНЫЕ СТУЛЬЯ'},
+]
+def read_json(way_to_file):
+        with open(way_to_file) as tables:
+            chairs_json = json.load(tables)
+        # print(chairs_json)
+        chairs_list = []
+        for section, commands in chairs_json.items():
+            chairs_list=commands
+        print(chairs_list)
+        return chairs_list
+# read_json(r'C:\Learning_Django\learn_django_beginner\lesson2\geekshop\tables.json')
+
+def index(request):
+    title = 'магазин'
+    tables = read_json(r'C:\Learning_Django\learn_django_beginner\lesson2\geekshop\tables.json')
+    context = {
+        'title': title,
+        'page_header': page_header,
+        'tables': tables,
+    }
+    return render(request, 'geekshop/index.html', context=context)
+
+
+def contacts(request):
+    context = {
+        'page_header': page_header
+    }
+    return render(request, 'geekshop/contact.html', context=context)
---
+++ geekshop/mainapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# class Menu(models.Model):
+#     name_menu_element = models.CharField(verbose_name = 'название элемента меню', max_length = 128)
+#
+#
+#     def __str__(self):
+#         return self.name_menu_element or f'Category with id - {self.pk}'
