091bf2037355ec4ad8f9d1f810e0fc5e79bd117a
Nikita <lukonenko.nikitka@mail.ru>
2021-08-07 23:01:46 +0300

    some fixes

--- authapp/urls.py
+++ authapp/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('register/', UserRegistrationView.as_view(), name='register'),
-    path('edit/<int:pk>', edit, name='edit'),
+    path('edit/', edit, name='edit'),
     path('verify/<str:email>/<str:activation_key>/', verify, name='verify')
     ]
